eb1395044f20e1e249273dd36cb899720e25e3d9
Colin Pade <colin@saucelabs.com>
2015-06-09 19:35:31 -0700

    does this fix jquery

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res) {
 
 app.get('/jQuery', function(req, res) {
   // ejs render automatically looks in the views folder
-  res.render('jQuery');
+  res.render('jquery');
 });
 
 
